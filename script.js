dc17e28d05cbe86c434b63b3fe6c9145bed7ffa5
SonavAgarwal <sonavagarwal@gmail.com>
2019-01-20 19:25:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function goAway(){
-    if (matchMedia('handheld').matches) {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         alert("You are viewing this page on a mobile device. For the best user experience, please use a laptop or pc. You can find some here: https://www.bestbuy.com/site/computers-pcs/laptop-computers/abcat0502000.c?id=abcat0502000")
     }
 }
